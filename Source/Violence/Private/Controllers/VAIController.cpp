24cac5b3cfb29fe6ff327b630b4437e31ed87190
Vee-Qor <vee.qor@gmail.com>
2025-09-21 14:01:20 +0300

    24. Setup Crowd Following Component

--- Source/Violence/Private/Controllers/VAIController.cpp
+++ Source/Violence/Private/Controllers/VAIController.cpp
@@ -4,13 +4,15 @@
 #include "Controllers/VAIController.h"
 
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Characters/VCharacter.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
-#include "Characters/VCharacter.h"
+#include "Navigation/CrowdFollowingComponent.h"
 
-AVAIController::AVAIController()
+AVAIController::AVAIController(const FObjectInitializer& ObjectInitializer) : Super(
+    ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
 {
-    AISightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("AI Sight Config");
+    AISightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("AISightConfig");
     AISightConfig->DetectionByAffiliation.bDetectEnemies = true;
     AISightConfig->DetectionByAffiliation.bDetectFriendlies = false;
     AISightConfig->DetectionByAffiliation.bDetectNeutrals = false;
@@ -19,7 +21,7 @@ AVAIController::AVAIController()
     AISightConfig->SetMaxAge(5.0f);
     AISightConfig->PeripheralVisionAngleDegrees = 90.0f;
 
-    AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AI Perception Component");
+    AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AIPerceptionComponent");
     AIPerceptionComponent->ConfigureSense(*AISightConfig);
     AIPerceptionComponent->SetDominantSense(UAISense_Sight::StaticClass());
     AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AVAIController::TargetPerceptionUpdated);
@@ -29,15 +31,17 @@ void AVAIController::OnPossess(APawn* InPawn)
 {
     Super::OnPossess(InPawn);
 
+    SetGenericTeamId(FGenericTeamId(3));
+
     if (AVCharacter* VCharacter = Cast<AVCharacter>(InPawn))
     {
         VCharacter->ServerInitial();
     }
 
-    SetGenericTeamId(FGenericTeamId(3));
-
-    check(BehaviorTree);
+    checkf(BehaviorTree, TEXT("Forgot assign BehaviorTree in %s"), *GetName());
     RunBehaviorTree(BehaviorTree);
+
+    InitCrowdSettings();
 }
 
 void AVAIController::TargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
@@ -45,4 +49,37 @@ void AVAIController::TargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus
     UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
     if (!BlackboardComponent) return;
     BlackboardComponent->SetValueAsObject(FName("Target"), Actor);
+}
+
+void AVAIController::InitCrowdSettings() const
+{
+    if (UCrowdFollowingComponent* CrowdComponent = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+    {
+        CrowdComponent->SetCrowdSimulationState(bEnableCrowdSimulation ? ECrowdSimulationState::Enabled : ECrowdSimulationState::Disabled);
+
+        switch (CrowdAvoidanceQuality)
+        {
+            case 1: CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Low);
+                break;
+            case 2: CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Medium);
+                break;
+            case 3: CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Good);
+                break;
+            case 4: CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);
+                break;
+        }
+
+        CrowdComponent->SetAvoidanceGroup(1);
+        CrowdComponent->SetGroupsToAvoid(1);
+
+        CrowdComponent->SetCrowdSeparation(true);
+        CrowdComponent->SetCrowdSeparationWeight(CrowdSeparationWeight);
+
+        CrowdComponent->SetCrowdAnticipateTurns(true);
+        CrowdComponent->SetCrowdObstacleAvoidance(true);
+
+        CrowdComponent->SetCrowdPathOffset(true);
+        CrowdComponent->SetCrowdCollisionQueryRange(CrowdCollisionQueryRange);
+        CrowdComponent->SetCrowdPathOptimizationRange(CrowdPathOptimizationRange);
+    }
 }
--- Source/Violence/Private/Controllers/VAIController.h
+++ Source/Violence/Private/Controllers/VAIController.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
-#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "VAIController.generated.h"
 
 class UAIPerceptionComponent;
@@ -16,19 +16,36 @@ class AVAIController : public AAIController
     GENERATED_BODY()
 
 public:
-    AVAIController();
+    AVAIController(const FObjectInitializer& ObjectInitializer);
     virtual void OnPossess(APawn* InPawn) override;
 
 private:
+    UPROPERTY(EditDefaultsOnly, Category = "AI")
+    UBehaviorTree* BehaviorTree;
+
     UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
     UAIPerceptionComponent* AIPerceptionComponent;
 
     UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
     UAISenseConfig_Sight* AISightConfig;
 
-    UPROPERTY(EditDefaultsOnly, Category = "AI")
-    UBehaviorTree* BehaviorTree;
-
     UFUNCTION()
     void TargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+    void InitCrowdSettings() const;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crowd")
+    bool bEnableCrowdSimulation = true;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crowd", meta=(ClampMin = "0", ClampMax = "4", EditCondition = "bEnableCrowdSimulation"))
+    int32 CrowdAvoidanceQuality = 4;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crowd", meta=(EditCondition="bEnableCrowdSimulation"))
+    float CrowdCollisionQueryRange = 600.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crowd", meta=(EditCondition = "bEnableCrowdSimulation"))
+    float CrowdPathOptimizationRange = 1000.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crowd", meta=(EditCondition = "bEnableCrowdSimulation"))
+    float CrowdSeparationWeight = 2.0f;
 };
